af1d85359366923bdc5b0b0c0d0519ec5b3b900e
Facundo <facundo.barboza.fernandez@fing.edu.uy>
2024-11-09 10:43:58 -0300

    cambios finales

---
+++ app_vf.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+from langchain_core.messages import HumanMessage, AIMessage
+
+from chatbot_pipeline import call_model
+
+app = Flask(__name__)
+
+chat_history = []
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Welcome to Chatbot Promtior! Use the endpoint '/chat' to interact."
+
+@app.route("/favicon.ico")
+def favicon():
+    return "", 204
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    global chat_history
+
+    # Obtain user message
+    user_input = request.json.get("message", "")
+
+    if not user_input:
+        return jsonify({"error": "No message provided"}), 400
+
+    # Prepare the state for model
+    state = {
+        "input": user_input,
+        "chat_history": chat_history,
+        "context": "",
+        "answer": ""
+    }
+
+    response = call_model(state)
+
+    # Update chat history
+    chat_history.extend([HumanMessage(user_input), AIMessage(response["answer"])])
+
+    return jsonify({
+        "answer": response["answer"]
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ chatbot_pipeline_vf.py
@@ -0,0 +1,149 @@
+import requests
+import os
+from langchain_openai import ChatOpenAI
+from typing import Sequence
+from bs4 import BeautifulSoup
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, StateGraph
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
+from langchain_community.document_loaders import PyPDFLoader
+
+
+langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = langchain_api_key
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+
+### Construct retriever ###
+
+base_url = "https://www.promtior.ai/"
+
+# Make request to page
+response = requests.get(base_url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Extract all links
+links = []
+for a_tag in soup.find_all("a", href=True):
+    href = a_tag["href"]
+    # Convert relative links to absolute
+    if href.startswith("/"):
+        href = base_url.rstrip("/") + href
+    if base_url in href:  # Make sure they are in the domain
+        links.append(href)
+
+unique_links = list(set(links))
+
+# webpage context
+loader = WebBaseLoader(web_paths=unique_links)
+web_docs = loader.load()
+
+# pdf context
+pdf_loader = PyPDFLoader("AIEngineer.pdf")
+pdf_docs = pdf_loader.load()
+
+# Combine both
+all_docs = web_docs + pdf_docs
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(all_docs)
+vectorstore = InMemoryVectorStore.from_documents(
+    documents=splits, embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+
+
+
+### Contextualize question ###
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+
+### Answer question ###
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+
+### Statefully manage chat history ###
+class State(TypedDict):
+    input: str
+    chat_history: Annotated[Sequence[BaseMessage], add_messages]
+    context: str
+    answer: str
+
+
+def call_model(state: State):
+    response = rag_chain.invoke(state)
+    return {
+        "chat_history": [
+            HumanMessage(state["input"]),
+            AIMessage(response["answer"]),
+        ],
+        "context": response["context"],
+        "answer": response["answer"],
+    }
+
+def call_model(state):
+    response = rag_chain.invoke(state)
+    return {
+        "chat_history": [
+            HumanMessage(state["input"]),
+            AIMessage(response["answer"]),
+        ],
+        "context": response["context"],
+        "answer": response["answer"],
+    }
+
+workflow = StateGraph(state_schema=State)
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
